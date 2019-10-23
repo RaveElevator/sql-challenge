ea65ba4cd65b987f9015482feeeb3023bbffb7c1
George McGuire <bigredlighter@yahoo.com>
2019-10-23 18:36:25 -0400

    7 and 8 update

---
+++ EmployeeSQL/QuickDBD-sql_challenge_dbd (1).sql
@@ -0,0 +1,34 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/SupgI9
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [departments] (
+
+)
+
+CREATE TABLE [dept_emp] (
+
+)
+
+CREATE TABLE [dept_manager] (
+
+)
+
+CREATE TABLE [employees] (
+
+)
+
+CREATE TABLE [salaries] (
+
+)
+
+CREATE TABLE [titles] (
+
+)
+
+COMMIT TRANSACTION QUICKDBD
